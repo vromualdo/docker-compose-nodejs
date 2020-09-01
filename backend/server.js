af26c1d5738c886af20aa14112b9a36cba071f84
vsromualdo <vsromualdo@gmail.com>
2020-09-01 14:12:44 -0300

    Body Parser - adicionando

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const restful = require('node-restful');
+const bodyParser = require('body-parser')
 const server = express();
 const mongoose = restful.mongoose;
 
@@ -8,8 +9,17 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://dbvagner/mydb');
 
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+server.use(bodyParser.json())
+
 server.get("/", (req, res)=>{
-    res.send("{result:'passou'}");
+
+    let result = {result:'passou', contend:{id:12, name:'XXX'}};
+
+    res.send(result);
 });
 
 server.listen(3000);
